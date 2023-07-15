816ababd99fdb78ea5ad931116b220d771f6f6a1
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2023-07-15 20:45:47 +0000

    integrate with admob: change build.gradle on both project and app level

--- lib/main.dart
+++ lib/main.dart
@@ -13,13 +13,7 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   final settingsController = SettingsController(SettingsService());
-
-  // Load the user's preferred theme while the splash screen is displayed.
-  // This prevents a sudden theme change when the app is first displayed.
   await settingsController.loadSettings();
 
-  // Run the app and pass in the SettingsController. The app listens to the
-  // SettingsController for changes, then passes it further down to the
-  // SettingsView.
   runApp(MyApp(settingsController: settingsController));
 }
