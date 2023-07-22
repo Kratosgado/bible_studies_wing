bccc13a3daad5e457fdd0c63dcfe49736c22a381
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2023-07-22 13:44:43 +0000

    display only one card at a time

--- lib/src/home/home_view.dart
+++ lib/src/home/home_view.dart
@@ -2,8 +2,6 @@ import 'package:bible_studies_wing/src/home/home_drawer.dart';
 import 'package:bible_studies_wing/src/home/verse_card.dart';
 import 'package:flutter/material.dart';
 
-import '../constants.dart';
-
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
@@ -11,23 +9,14 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final cardList = [verseCard(), verseCard(), verseCard(), verseCard(), verseCard()];
-
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('The Word'),
-        ),
-        drawer: homeDrawer(context),
-        body: SingleChildScrollView(
-          padding: const EdgeInsets.all(10),
-          child: Container(
-            decoration: homeGradient,
-            child: Center(
-              child: Column(
-                children: cardList,
-              ),
-            ),
-          ),
-        ));
+      appBar: AppBar(
+        title: const Text('The Word'),
+      ),
+      drawer: homeDrawer(context),
+      body: Center(
+        child: verseCard(context),
+      ),
+    );
   }
 }
--- lib/src/home/verse_card.dart
+++ lib/src/home/verse_card.dart
@@ -1,27 +1,38 @@
 import 'package:flutter/material.dart';
 
-Widget verseCard() {
-  return SingleChildScrollView(
-    scrollDirection: Axis.horizontal,
-    padding: const EdgeInsets.only(bottom: 10),
-    child: Row(
-      children: [
-        SizedBox(
-          height: 200,
-          width: 350,
-          child: Card(
-            color: Colors.purple,
-            elevation: 10,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15.0),
-            ),
-            child: Image.asset(
+Widget verseCard(BuildContext context) {
+  return SizedBox(
+    height: 300,
+    width: 350,
+    child: Card(
+        shadowColor: Colors.purple,
+        elevation: 10,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image.asset(
               'assets/images/cards/card1.jpeg',
               fit: BoxFit.contain,
             ),
-          ),
-        )
-      ],
-    ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.share),
+                  iconSize: 35,
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.message_rounded),
+                  iconSize: 35,
+                )
+              ],
+            ),
+          ],
+        )),
   );
 }
